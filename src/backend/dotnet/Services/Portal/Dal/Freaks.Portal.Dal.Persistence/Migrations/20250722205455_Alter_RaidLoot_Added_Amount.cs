d07215a900c18f79bfdf49986704306a3f8af00a
Andrey Dvoretskikh <a.dvoretskikh@yandex.ru>
2025-07-23 01:57:00 +0500

    feat: - added missed migration

---
+++ src/backend/dotnet/Services/Portal/Dal/Freaks.Portal.Dal.Persistence/Migrations/20250722205455_Alter_RaidLoot_Added_Amount.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Freaks.Portal.Dal.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_RaidLoot_Added_Amount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "amount",
+                schema: "portal",
+                table: "raid_loot",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "amount",
+                schema: "portal",
+                table: "raid_loot");
+        }
+    }
+}
