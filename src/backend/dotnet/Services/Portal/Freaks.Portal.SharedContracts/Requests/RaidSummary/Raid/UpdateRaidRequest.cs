e3b11c7b47e9165c97cafcc22cac8dd95e1ba51a
Andrey Dvoretskikh <a.dvoretskikh@yandex.ru>
2025-09-16 18:00:00 +0500

    task: add app-version controller

--- src/backend/dotnet/Services/Portal/Freaks.Portal.SharedContracts/Requests/RaidSummary/Raid/UpdateRaidRequest.cs
+++ src/backend/dotnet/Services/Portal/Freaks.Portal.SharedContracts/Requests/RaidSummary/Raid/UpdateRaidRequest.cs
@@ -6,7 +6,6 @@ namespace Freaks.Portal.SharedContracts.Requests.RaidSummary.Raid;
 ///     Запрос на обновление информации о рейде.
 ///     Содержит новые значения для типа босса, формата и описания.
 /// </summary>
-/// <param name="Id">Идентификатор рейда, который необходимо обновить.</param>
 /// <param name="BossType">Новый тип босса.</param>
 /// <param name="FormatType">Новый формат рейда.</param>
 /// <param name="Description">Обновлённое описание рейда.</param>
