3097a8953337897e0d3341b637928b86029a6e5f
Andrey Dvoretskikh <a.dvoretskikh@yandex.ru>
2026-01-14 16:35:04 +0500

    some fixes

--- docker/run_docker-compose-prod.sh
+++ docker/run_docker-compose-prod.sh
@@ -1,3 +1,3 @@
-docker-compose -f ./docker-compose-back-base.yml build
-docker-compose -f ./docker-compose-prod.yml up --build -d --remove-orphans
+docker compose -f ./docker-compose-back-base.yml build
+docker compose -f ./docker-compose-prod.yml up --build -d --remove-orphans
 pause
